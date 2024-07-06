dd831fb9e0c6454fcbd80e35d47399a3381e9fde
nagasai <nagasaigubbala544@gmail.com>
2024-07-06 19:28:06 +0530

    first commit

---
+++ FRONTEND/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+        At Envision Events, we transform your dreams into reality. Whether you’re planning a lavish wedding, a corporate gala, a milestone birthday, or an intimate gathering, our team of experienced event planners is dedicated to creating unforgettable experiences tailored to your vision.we believe every event should be unique, memorable, and flawlessly executed. With years of experience and a passion for perfection, we specialize in transforming your visions into reality. Whether you're planning a wedding, corporate event, or private celebration, our team is dedicated to making your occasion extraordinary.
+      </p>
+    </section>
+  );
+};
+
+export default About;
